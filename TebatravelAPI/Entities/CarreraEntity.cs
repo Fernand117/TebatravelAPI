e6d491f4eae1bff30950456a99586b81ce5ab277
Luis Fernando Leyva Luna <fernando_rendel892@hotmail.com>
2025-04-08 11:25:25 -0600

    feat(config-entities):se configuran las entidades a utilizar, además se añaden paquetes nugget a utilizar

---
+++ TebatravelAPI/Entities/CarreraEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace TebatravelAPI.Entities
+{
+    public class CarreraEntity
+    {
+        public int CarreraId { get; set; }
+        public string NombreCarrera { get; set; } = string.Empty;
+        public AlumnoEntity Alumno { get; set; } = new AlumnoEntity();
+    }
+}
